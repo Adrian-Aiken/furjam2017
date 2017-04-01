65dafd889670b2ff343a2b7ca862967c6eed4bc2
Eric Kenvin <zarawyk@gmail.com>
2017-04-01 13:11:09 -0700

    Derp, syntax error.  Also rearranged gamepad mapping's prototype

--- app/Gamepad.js
+++ app/Gamepad.js
@@ -35,43 +35,6 @@ var GamepadMapping = function(_buttons, _axes) {
     this.deadZoneValue = 0.08;
 }
 
-/** 
- * Constructor for Gamepad object
- *  GamepadNumber and GamepadManager only needed if isKeyboard is false
- */
-var PlayerGamepad = function(_isKeyboard, _defaultConfig, _gamepadNumber, _gamepadManager) {
-    this.isKeyboard = _isKeyboard;
-    this.config = _defaultConfig;
-    this.gamepadNum = _gamepadNumber;
-    this.gamepadManager = _gamepadManager;
-}
-
-PlayerGamepad.prototype = {
-    left:   function () { return this.isKeyboard ? this.getButton(this.config.buttons.DpadLeft) || this.getAxis(this.config.axes.LeftStickHorizontal) < (this.config.deadZoneValue * -1) : this.getButton(0); },
-    up:     function () { return this.isKeyboard ? this.getButton(this.config.buttons.DpadUp) || this.getAxis(this.config.axes.LeftStickVertical) > this.config.deadZoneValue : this.getButton(1);  },
-    right:  function () { return this.isKeyboard ? this.getButton(this.config.buttons.DpadRight) || this.getAxis(this.config.axes.LeftStickHorizontal) > this.config.deadZoneValue : this.getButton(2);  },
-    down:   function () { return this.isKeyboard ? this.getButton(this.config.buttons.DpadDown) || this.getAxis(this.config.axes.LeftStickVertical) < (this.config.deadZoneValue * -1) : this.getButton(3);  },
-    a_btn:  function () { return this.isKeyboard ? this.getButton(this.config.buttons.A) : this.getButton(4);  },
-    b_btn:  function () { return this.isKeyboard ? this.getButton(this.config.buttons.B) : this.getButton(5);  },
-
-    getButton: function (buttonNum) {
-        if (this.isKeyboard) {
-            return keyMap[this.config[buttonNum]]; 
-        } else {
-            return this.gamepadManager.getButtonPressed(this.gamepadNum, this.config.buttons[buttonNum]);
-        }
-    }
-
-    getAxis: function(axisNum) {
-        if(this.isKeyboard){
-            return 0;
-        }
-        else{
-            return this.gamepadManager.getAxisValue(this.gamepadNum, this.config.axes[axisNum]);
-        }
-    }
-} 
-
 GamepadMapping.prototype = {
 
     DefaultGamepadButtons:{
@@ -113,6 +76,43 @@ GamepadMapping.prototype = {
         }
     }
 }
+
+/** 
+ * Constructor for Gamepad object
+ *  GamepadNumber and GamepadManager only needed if isKeyboard is false
+ */
+var PlayerGamepad = function(_isKeyboard, _defaultConfig, _gamepadNumber, _gamepadManager) {
+    this.isKeyboard = _isKeyboard;
+    this.config = _defaultConfig;
+    this.gamepadNum = _gamepadNumber;
+    this.gamepadManager = _gamepadManager;
+}
+
+PlayerGamepad.prototype = {
+    left:   function () { return this.isKeyboard ? this.getButton(this.config.buttons.DpadLeft) || this.getAxis(this.config.axes.LeftStickHorizontal) < (this.config.deadZoneValue * -1) : this.getButton(0); },
+    up:     function () { return this.isKeyboard ? this.getButton(this.config.buttons.DpadUp) || this.getAxis(this.config.axes.LeftStickVertical) > this.config.deadZoneValue : this.getButton(1);  },
+    right:  function () { return this.isKeyboard ? this.getButton(this.config.buttons.DpadRight) || this.getAxis(this.config.axes.LeftStickHorizontal) > this.config.deadZoneValue : this.getButton(2);  },
+    down:   function () { return this.isKeyboard ? this.getButton(this.config.buttons.DpadDown) || this.getAxis(this.config.axes.LeftStickVertical) < (this.config.deadZoneValue * -1) : this.getButton(3);  },
+    a_btn:  function () { return this.isKeyboard ? this.getButton(this.config.buttons.A) : this.getButton(4);  },
+    b_btn:  function () { return this.isKeyboard ? this.getButton(this.config.buttons.B) : this.getButton(5);  },
+
+    getButton: function (buttonNum) {
+        if (this.isKeyboard) {
+            return keyMap[this.config[buttonNum]]; 
+        } else {
+            return this.gamepadManager.getButtonPressed(this.gamepadNum, this.config.buttons[buttonNum]);
+        }
+    },
+
+    getAxis: function(axisNum) {
+        if(this.isKeyboard){
+            return 0;
+        }
+        else{
+            return this.gamepadManager.getAxisValue(this.gamepadNum, this.config.axes[axisNum]);
+        }
+    }
+} 
 
 var GamepadManager = function(){
     window.addEventListener("gamepadconnected", this.gamepadConnected.bind(this), false);
